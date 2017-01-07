b7fa9f718309800f5892191df6a3ea5683535bff
marcos855 <marcos-855@hotmail.com>
2017-01-07 11:37:34 +0100

    renombramiento

---
+++ dialogo/Assets/scr.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class scr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		string text = System.IO.File.ReadAllText("myfile.txt");
+	
+		Debug.Log (text);
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+}
