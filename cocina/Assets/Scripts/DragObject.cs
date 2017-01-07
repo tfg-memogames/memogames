5ca088fd4994faf08c55caa0ff164ac199710f4a
Javier Lopez <javlop05@ucm.es>
2017-01-07 20:38:30 +0100

    recetas V2

---
+++ cocina/Assets/Scripts/DragObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class DragObject: MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	[HideInInspector]
+	public Vector2 returnPoint;
+
+	void Start() {
+		this.gameObject.AddComponent<CanvasGroup>();
+	}
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("Begin drag");
+
+		returnPoint = this.transform.position;
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		//Debug.Log ("Dragging");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("End drag");
+
+		this.transform.position = returnPoint;
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+		
+}
---
+++ cocina/Assets/Scripts/DropObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class DropObject : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log("Pointer enter " + this.name);
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log ("Pointer exit " + this.name);
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log("Drop on " + this.name);
+
+		DragObject d = eventData.pointerDrag.GetComponent<DragObject> ();
+
+		if (d != null) {
+			d.returnPoint = eventData.position;
+		}
+	}
+
+}
