41b3e6089675b83bc5459b9f8c69186bbca1c2c1
Javier Lopez <javlop05@ucm.es>
2017-01-08 20:25:35 +0100

    Recetas v3.1

--- cocina/Assets/Scripts/PlayerClick.cs
+++ cocina/Assets/Scripts/PlayerClick.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 
 public class PlayerClick : MonoBehaviour {
 
+	//Dragging attributes
+	private bool dragging;
+	private GameObject gameObjectToDrag;
+	private Transform startPosition;
+	private Vector3 touchPosition;
+	private Vector3 offset;
+	private Vector3 newCenterPosition;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -27,8 +35,29 @@ public class PlayerClick : MonoBehaviour {
 				} else if (tag == "Tap") {
 					Tap t = hitInfo.collider.gameObject.GetComponent<Tap> ();
 					t.OpenCloseTap ();
+
+				} else if (tag == "Item") {
+					gameObjectToDrag = hitInfo.collider.gameObject;
+					startPosition = gameObjectToDrag.transform;
+					touchPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+					offset = touchPosition - startPosition.position;
+					dragging = true;
 				}
 			}
+		}
+
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			if (dragging) {
+				touchPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				newCenterPosition = touchPosition - offset;
+				gameObjectToDrag.transform.position = new Vector3 (newCenterPosition.x, newCenterPosition.y, 0);
+			}
+		}
+
+		//End of dragging
+		if (Input.GetKeyUp (KeyCode.Mouse0)) {
+			if (dragging) 
+				dragging = false;
 		}
 	}
 }
