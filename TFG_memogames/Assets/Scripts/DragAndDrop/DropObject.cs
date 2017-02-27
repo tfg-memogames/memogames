6c85d441a3658264798f72c5aaa7db4a429b219c
marquitos855 <laina855@gmail.com>
2017-02-27 10:57:51 +0100

    objects

---
+++ TFG_memogames/Assets/Scripts/DragAndDrop/DropObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class DropObject : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+
+	public void OnPointerEnter(PointerEventData eventData) {
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+
+		DragObject d = eventData.pointerDrag.GetComponent<DragObject> ();
+
+		if (d != null) {
+			d.returnPoint = eventData.position;
+			d.dropZone = true;
+			d.parent = this.transform;
+		}
+	}
+
+}
