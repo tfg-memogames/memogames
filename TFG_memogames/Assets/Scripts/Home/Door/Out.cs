dbd315f41a6900a3d449b9e2ef9b703aa34f8c8b
marquitos855 <laina855@gmail.com>
2017-02-28 00:12:45 +0100

    caamera controller

---
+++ TFG_memogames/Assets/Scripts/Home/Door/Out.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Out : MonoBehaviour {
+
+	public string scene;
+
+	void Start(){
+		Button btn = this.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		SceneManager.LoadScene(scene);
+	}
+}
