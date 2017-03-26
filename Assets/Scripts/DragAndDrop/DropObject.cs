b8e613251739d97838b56a209e0a75544abf70fe
mlaina <laina855@gmail.com>
2017-03-26 11:22:41 +0200

    ya no se que decir

--- Assets/Scripts/DragAndDrop/DropObject.cs
+++ Assets/Scripts/DragAndDrop/DropObject.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
 public class DropObject : MonoBehaviour {
 
 	public void ItemWasDropped(GameObject go)
     {
-        Debug.Log(go.name + " drag on " + name);
-
-        FindObjectOfType<RecipeManager>().ItemWasDropped(go, gameObject);
+        gameObject.SetActive(false);
     }
 }
