0e344d2ce7f72b21eea81899e7f73a8e736441f3
mlaina <laina855@gmail.com>
2017-03-27 13:38:56 +0200

    camera

---
+++ Assets/Scripts/Home/Door/Back.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour {
+
+	public string scene;
+	// Use this for initialization
+	void Start () {
+		Button btn = this.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);	
+	}
+
+	void TaskOnClick(){
+		SceneManager.LoadScene(scene);
+	}
+}
+
+
---
+++ Assets/Scripts/PickUp/DropObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DropObject : MonoBehaviour {
+
+	public void ItemWasDropped(GameObject go)
+    {
+        Debug.Log(go.name + " drag on " + name);
+
+        FindObjectOfType<RecipeManager>().ItemWasDropped(go, gameObject);
+    }
+}
