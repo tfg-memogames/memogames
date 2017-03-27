73d03eda6801b46788221fa9cd1a2226542e74c6
mlaina <laina855@gmail.com>
2017-03-27 19:02:15 +0200

    son for pickup

---
+++ Assets/Scripts/Home/PickUp/PickUpManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PickUpManager : MonoBehaviour {
+	private int totalObjects;
+	private static string file="house.json";
+	//array objeto lugar en el que se encuentra
+
+	private class HouseObject{
+		public string ob;
+		public string site;
+	}
+
+	private class Room{
+		public HouseObject [] room;
+	}
+
+	private class House{
+		public Room [] house;
+	}
+
+
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject);
+		initObjects ();
+	
+	
+	}
+
+	void initObjects(){
+		string h;
+
+		string json=LoadJSONData ();
+
+		h=JsonUtility.FromJson<string>(json);
+		Debug.Log (h);
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+
+	public static string LoadJSONData(){
+
+		string filePath=Path.Combine(Application.streamingAssetsPath, file);
+		string json="";
+
+		if (File.Exists (filePath)) {
+			json = File.ReadAllText (filePath);
+		}
+			
+		return json;
+	}
+
+}
