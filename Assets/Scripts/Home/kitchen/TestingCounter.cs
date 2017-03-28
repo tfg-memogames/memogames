adadff83c3fe2a0b92abefa05a5135a4d151cca4
javlop05 <javlop05@ucm.es>
2017-03-28 11:53:07 +0200

    Timer&success&error effect
    Script for kitchen timer
    Script for instance error or success animation via script

---
+++ Assets/Scripts/Home/kitchen/TestingCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TestingCounter : MonoBehaviour {
+
+    public Text timeToGo;
+    public float time;
+
+    private Image image;
+    private float restTime;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+        timeToGo.text = "" + (int)time;
+        restTime = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        restTime -= Time.deltaTime;
+        image.fillAmount = restTime / time;
+        timeToGo.text = "" + (int)restTime;
+        if (restTime <= 0) Debug.Log("GameOver");
+    }
+}
---
+++ Assets/Scripts/Home/kitchen/shadowEffect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Transform))]
+[RequireComponent(typeof(Image))]
+public class shadowEffect : MonoBehaviour {
+
+    public float timeEffect;
+    public Vector3 finalPosition;
+
+    private Image image;
+
+    private Color startColor;
+    private Vector3 startPosition;
+
+    private Vector3 posTransition;
+    private float timeToFinish;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        startColor = image.color;
+        startPosition = transform.localPosition;
+        posTransition = finalPosition - startPosition;
+        timeToFinish = timeEffect;
+        StartCoroutine(WaitAndDestroy());
+    }
+
+    private IEnumerator WaitAndDestroy()
+    {
+        yield return new WaitForSeconds(timeEffect);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        timeToFinish -= Time.deltaTime;
+        float percOfCurrTime = (timeEffect - timeToFinish) / timeEffect;
+
+        transform.localPosition = new Vector3(startPosition.x + posTransition.x * percOfCurrTime,
+                                         startPosition.y + posTransition.y * percOfCurrTime,
+                                         startPosition.z + posTransition.z * percOfCurrTime);
+
+        image.color = new Color(startColor.r,
+                                startColor.g,
+                                startColor.b,
+                                startColor.a * timeToFinish / timeEffect);
+    }
+
+}
