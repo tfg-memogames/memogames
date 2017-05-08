37adbc7ff6fa2b2dbb5142fa57181a21a3bcb923
Aarón Durán <aarondur@ucm.es>
2017-05-08 21:17:34 +0200

    Form made

---
+++ Assets/Scripts/FormManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FormManager : MonoBehaviour {
+
+
+    public InputField _input;
+    private GameState _gs;
+
+    // Use this for initialization
+    void Start () {
+        this._gs = GameObject.FindObjectOfType<GameState>();
+
+    }
+	
+	public void clickStart()
+    {
+        this._gs.name = _input.text;
+        SceneManager.LoadScene("Level_Selector");
+    }
+}
