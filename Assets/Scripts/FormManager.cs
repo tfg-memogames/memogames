94aab3c219f4fc2fb012c3a12bf9121d15812790
javlop05 <javlop05@ucm.es>
2017-05-09 20:07:41 +0200

    Form changes

--- Assets/Scripts/FormManager.cs
+++ Assets/Scripts/FormManager.cs
@@ -6,19 +6,40 @@ using UnityEngine.UI;
 
 public class FormManager : MonoBehaviour {
 
-
-    public InputField _input;
-    private GameState _gs;
+    public InputField _inputName;
+    public InputField _inputSurname;
+    public Text _messageError;
+    //private GameState _gs;
 
     // Use this for initialization
     void Start () {
-        this._gs = GameObject.FindObjectOfType<GameState>();
-
+        //this._gs = GameObject.FindObjectOfType<GameState>();
+        this._messageError.gameObject.SetActive(false);
     }
 	
-	public void clickStart()
+	public void ClickStart()
     {
-        this._gs.name = _input.text;
-        SceneManager.LoadScene("Level_Selector");
+
+        if (this._inputName.text == "")
+        {
+            DisplayErrorMessage("Debes introducir un nombre");
+            return;
+        }
+
+        if (this._inputSurname.text == "")
+        {
+            DisplayErrorMessage("Debes introducir un apellido");
+            return;
+        }
+
+        //this._gs.name = _inputName.text;
+        //this._gs.surname = _inputSurname.text;
+        //SceneManager.LoadScene("Level_Selector");
     }
-}
+
+    private void DisplayErrorMessage(string text)
+    {
+        this._messageError.gameObject.SetActive(true);
+        this._messageError.text = text;
+    }
+ }
