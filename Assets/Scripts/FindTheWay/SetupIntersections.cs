8a2444cd60a2b9b4e5387eea28fa0982a4a904a0
Aarón Durán <aarondur@ucm.es>
2017-05-13 15:26:15 +0200

    Escala de las flechas solucionado

--- Assets/Scripts/FindTheWay/SetupIntersections.cs
+++ Assets/Scripts/FindTheWay/SetupIntersections.cs
@@ -12,6 +12,11 @@ public class SetupIntersections : MonoBehaviour {
 
         GameObject[] arrows = Resources.LoadAll<GameObject>(path);
 
+        GameObject arrow_NE = Resources.Load<GameObject>(path + "/Arrow_NE");
+        GameObject arrow_NW = Resources.Load<GameObject>(path + "/Arrow_NW");
+        GameObject arrow_SE = Resources.Load<GameObject>(path + "/Arrow_SE");
+        GameObject arrow_SW = Resources.Load<GameObject>(path + "/Arrow_SW");
+
         GameObject[] intersections = GameObject.FindGameObjectsWithTag("I");		
         foreach (GameObject i in intersections)
         {
@@ -21,37 +26,37 @@ public class SetupIntersections : MonoBehaviour {
             {
                 GameObject child = i.transform.GetChild(count).gameObject;
                 child.AddComponent<Arrow>();
-                //child.AddComponent<PolygonCollider2D>();
+                
 
                 if (child.name == dirs.NE.ToString())
                 {
-                    GameObject a = Instantiate(arrows[0], child.transform);
+                    GameObject a = Instantiate(arrow_NE, child.transform);
                     a.transform.localPosition = Vector3.zero;
-                    a.transform.localScale = a.transform.localScale.normalized * 3;
+                    
                     a.SetActive(false);
                     inter.AddArrow(a);
 
                 } else if (child.name == dirs.NW.ToString())
                 {
-                    GameObject a = Instantiate(arrows[1], child.transform);
+                    GameObject a = Instantiate(arrow_NW, child.transform);
                     a.transform.localPosition = Vector3.zero;
-                    a.transform.localScale = Vector3.one * 3;
+                    
                     a.SetActive(false);
                     inter.AddArrow(a);
                 }
                 else if (child.name == dirs.SE.ToString())
                 {
-                    GameObject a = Instantiate(arrows[2], child.transform);
+                    GameObject a = Instantiate(arrow_SE, child.transform);
                     a.transform.localPosition = Vector3.zero;
-                    a.transform.localScale = Vector3.one * 3;
+                    
                     a.SetActive(false);
                     inter.AddArrow(a);
                 }
                 else if (child.name == dirs.SW.ToString())
                 {
-                    GameObject a = Instantiate(arrows[3], child.transform);
+                    GameObject a = Instantiate(arrow_SW, child.transform);
                     a.transform.localPosition = Vector3.zero;
-                    a.transform.localScale = Vector3.one * 3;
+                    
                     a.SetActive(false);
                     inter.AddArrow(a);
                 }
