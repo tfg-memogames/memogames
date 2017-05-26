bcb425bdfcd9fcb4096ea8c4a5729f7250f33e18
mlaina <laina855@gmail.com>
2017-05-26 21:20:44 +0200

    menu, zoom

---
+++ Assets/Scripts/Coffee Shop/Zoom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+
+	public GameObject obj;
+	public int speed;
+	public int near;
+
+	private bool zm=true;
+	private Vector3 o;
+	private float zIniCamera;
+
+
+	void Start(){
+		o = obj.GetComponent<Transform> ().position;
+		zIniCamera = Camera.main.GetComponent<Transform> ().position.z;
+	}
+
+	public void zoom(){
+		zm = !zm;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (zm) {
+			Vector3 v = Camera.main.GetComponent<Transform> ().position;
+
+			Camera.main.GetComponent<Transform> ().position = new Vector3 (o.x, o.y, v.z + 0.01F * speed);
+		
+			if (v.z > ((zIniCamera + o.z) / near))
+				zoom ();
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void iniGame(){
+		SceneManager.LoadScene("Conver With Fernando");
+	}
+	public void Credits(){
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void Config(){
+		SceneManager.LoadScene("Config");
+	}
+
+	public void Back(){
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
