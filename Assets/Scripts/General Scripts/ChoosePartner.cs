8d72d20ecc0c6e038f9a0eb7814709d3bcfa7851
Aarón Durán <aaron.duran@babel.es>
2017-05-29 21:09:43 +0200

    Instancia del sexo contrario implementada

---
+++ Assets/Scripts/General Scripts/ChoosePartner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoosePartner : MonoBehaviour {
+
+    private const string wifePath = "Sprites/Characters/Wife/completedWife";
+    private const string husbandPath = "Sprites/Characters/Husband/completedHusband";
+
+    private GameState _gs;
+    private GameObject _partner;
+    private Sprite _sprite;
+
+    // Use this for initialization
+
+
+    //No se puede instanciar un prefab porque en cada escena necesitará unos scripts distintos
+    void Start () {
+        this._gs = GameObject.FindObjectOfType<GameState>();
+        this._partner = GameObject.Find("Partner");
+
+        if (_gs.playerGender == GameState.Gender.M)
+            this._sprite = Resources.Load<Sprite>(wifePath);
+        else
+            this._sprite = Resources.Load<Sprite>(husbandPath);
+
+        SpriteRenderer sp = this._partner.GetComponent<SpriteRenderer>();
+       
+        sp.sprite = this._sprite;
+        Instantiate(this._partner);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
