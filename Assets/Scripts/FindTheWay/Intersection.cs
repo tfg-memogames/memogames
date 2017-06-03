edd69bbe9a6b94f4fe960143174982d9b74759f2
Aarón Durán <aaron.duran@babel.es>
2017-06-03 14:18:36 +0200

    Arreglado bug de ftw

--- Assets/Scripts/FindTheWay/Intersection.cs
+++ Assets/Scripts/FindTheWay/Intersection.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Intersection : MonoBehaviour {
+public class Intersection : MonoBehaviour
+{
 
     private List<GameObject> arrows;
+    private string lastTag;
+    private GameObject lastRoad;
 
-	// Use this for initialization
-	void Awake () {
-        arrows = new List<GameObject>();
-	}
-
-	public void ShowArrows(GameObject touch)
+    // Use this for initialization
+    void Awake()
     {
-        foreach(GameObject go in arrows)
+        arrows = new List<GameObject>();
+    }
+
+    public void ShowArrows(GameObject touch)
+    {
+        foreach (GameObject go in arrows)
         {
             if (go.transform.parent.gameObject != touch)
                 go.SetActive(true);
@@ -22,7 +26,7 @@ public class Intersection : MonoBehaviour {
 
     public void HideArrows()
     {
-        foreach(GameObject go in arrows)
+        foreach (GameObject go in arrows)
         {
             go.SetActive(false);
         }
@@ -33,26 +37,60 @@ public class Intersection : MonoBehaviour {
         arrows.Add(go);
     }
 
+    public void returnTag()
+    {
+        //GameObject.Find(this.lastRoad);
+    }
+
     public void ChangeTag(string newTag, CarMove.Direction dir)
     {
-        switch(dir)
+        //Para cambiar el tag, debemos guardar el que tenía anteriormente y devolvérselo en algún momento.
+        switch (dir)
         {
             case CarMove.Direction.NW:
                 //Busco a mi hijo SE
+                this.lastRoad = transform.FindChild("SE").gameObject;
+                this.lastTag = this.lastRoad.tag;
+
                 transform.FindChild("SE").gameObject.tag = newTag;
                 break;
             case CarMove.Direction.SE:
                 //Busco a mi hijo NW
+                this.lastRoad = transform.FindChild("NW").gameObject;
+                this.lastTag = this.lastRoad.tag;
+
                 transform.FindChild("NW").gameObject.tag = newTag;
                 break;
             case CarMove.Direction.NE:
                 //Busco a mi hijo SW
+                this.lastRoad = transform.FindChild("SW").gameObject;
+                this.lastTag = this.lastRoad.tag;
+
+
                 transform.FindChild("SW").gameObject.tag = newTag;
                 break;
             case CarMove.Direction.SW:
                 //Busco a mi hijo NE
+
+                this.lastRoad = transform.FindChild("NE").gameObject;
+                this.lastTag = this.lastRoad.tag;
+
+
                 transform.FindChild("NE").gameObject.tag = newTag;
                 break;
+        }
+        Debug.Log(lastTag);
+    }
+
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+        //Cuando salga el coche de la intersección cambiamos 
+        if(other.tag == "Car") { 
+            
+            this.lastRoad.tag = this.lastTag;
         }
     }
 }
