5d39c399e695e1638871cbf008d6432b824da4bd
Rafaelml <rafaelmm@ucm.es>
2018-01-03 03:52:59 +0100

    Efecto_cursor
    Cursor para seleccionar botones

---
+++ Assets/Scripts/FindTheWay/MouseOverEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverEffect : MonoBehaviour {
+    public Texture2D cursorTexture;
+    private CursorMode cursorMode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
+    public void PointerEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    public void PointerExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
