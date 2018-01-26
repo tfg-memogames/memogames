aa00b20768acd78cd758f5423173e9d64df38e52
Rafaelml <rafaelmm@ucm.es>
2018-01-26 15:37:50 +0100

    tracker async
    Cambiando el tracker para poder probar ftw

--- Assets/Tracker/csharp-tracker/TrackerAsset/TrackerAsset.cs
+++ Assets/Tracker/csharp-tracker/TrackerAsset/TrackerAsset.cs
@@ -15,9 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-//#define ASYNC
-#undef ASYNC
+#define ASYNC
+///#undef ASYNC
 
 namespace AssetPackage
 {
