4e24d72f141edea73967ed9d2e43fe5082e7df31
DavidCogolludo <davipe05@ucm.es>
2018-02-25 20:03:39 +0100

    Añadido menu inicio 15 Objects

---
+++ Assets/Scripts/15Objects/changeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeScene(string scene)
+    {
+        if(scene != "exit")
+            SceneManager.LoadScene(scene);
+        else
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+}
