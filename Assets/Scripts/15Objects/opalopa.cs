0c6562a8beb9ecfa7c6874098a46a1c01c8dfc2d
DavidCogolludo <davipe05@ucm.es>
2018-02-28 12:34:28 +0100

    Primera versión tutorial 15 objects

---
+++ Assets/Scripts/15Objects/opalopa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class opalopa : MonoBehaviour
+{
+
+    // Use this for initialization
+    private SortedDictionary<string, int> diccionary;
+
+
+    public string[] words; 
+    void Start()
+    {
+       
+    }
+
+    public string[]  dameDic(out int id)
+    {
+        id = 0;
+        for (int i = 0; i < this.name.Length; i++) id += (int)this.name[i];
+        return words;
+    }
+    void Update()
+    {
+       
+    }
+
+}
--- Assets/Scripts/DragAndDrop/DragObject.cs
+++ Assets/Scripts/DragAndDrop/DragObject.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 
